e029a09c852d07b031a1b0de1221de5d9f443316
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-05-10 14:50:23 -0500

    Added express and backend routes for user registration

---
+++ backend/controllers/userController.js
@@ -0,0 +1,59 @@
+const asyncHandler = require("express-async-handler");
+const bcyrpt = require("bcryptjs");
+
+// User model for db
+const User = require("../models/userModel");
+
+// @desc register a new user
+// @route /api/users
+// @access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  //Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hash pass
+  const salt = await bcyrpt.genSalt(10);
+  const hashedPassword = await bcyrpt.hash(password, salt);
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc login user
+// @route /api/users/login
+// @access public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login Route");
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 8000;
+
+//connect to DB
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// User Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
