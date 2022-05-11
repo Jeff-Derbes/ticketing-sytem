b358cacf70caaf6712aa08f9633e6c435ed39f18
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-05-10 21:18:27 -0500

    finished backend authentication and ran create-react-app to begin frontned

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,6 +72,18 @@ const loginUser = asyncHandler(async (req, res) => {
   res.send("Login Route");
 });
 
+// @desc Get current user
+// @route /api/users/me
+// @access private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
@@ -81,4 +93,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
