fafbacdcd06387852eec135e78d63d6802057b50
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-05-16 22:42:43 -0500

    finished login feature

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
-import { FaSignInAlt } from "react-icons/fa";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { FaSignInAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
-import { login } from "../features/auth/authSlice";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -10,13 +12,27 @@ function Login() {
     password: "",
   });
 
-  const { name, email, password, password2 } = formData;
+  const { email, password } = formData;
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    // Redirect when logged in
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -36,13 +52,17 @@ function Login() {
     dispatch(login(userData));
   };
 
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <section className="heading">
         <h1>
           <FaSignInAlt /> Login
         </h1>
-        <p>Please login</p>
+        <p>Please log in to get support</p>
       </section>
 
       <section className="form">
@@ -59,7 +79,6 @@ function Login() {
               required
             />
           </div>
-
           <div className="form-group">
             <input
               type="password"
@@ -68,11 +87,10 @@ function Login() {
               name="password"
               value={password}
               onChange={onChange}
-              placeholder="Enter your password"
+              placeholder="Enter password"
               required
             />
           </div>
-
           <div className="form-group">
             <button className="btn btn-block">Submit</button>
           </div>
