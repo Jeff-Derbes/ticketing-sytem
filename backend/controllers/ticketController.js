5b9d69143c8499db4b6bc3b0192cf48528b79e03
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-05-17 22:44:16 -0500

    finished CRUD functionality of tickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -49,4 +49,87 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
-module.exports = { createTicket, getTickets };
+// @desc   Get single ticket
+// @route  GET /api/tickets/:id
+// @access  Private
+const getTicket = asyncHandler(async (req, res) => {
+  //Get user using id and jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("TIcket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  res.status(200).json(ticket);
+});
+
+// @desc   delete single ticket
+// @route  DELETE /api/tickets/:id
+// @access  Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  //Get user using id and jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  await Ticket.remove(req.params.id);
+
+  res.status(200).json({ success: true });
+});
+
+// @desc   update single ticket
+// @route  PUT /api/tickets/:id
+// @access  Private
+const updateTicket = asyncHandler(async (req, res) => {
+  //Get user using id and jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("TIcket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updateTicket);
+});
+
+module.exports = {
+  createTicket,
+  getTickets,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+};
